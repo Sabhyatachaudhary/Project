be59383e50837f1b0f1adbf3f30dbf49cc87375f
Sabhyatachaudhary <88722683+Sabhyatachaudhary@users.noreply.github.com>
2022-08-24 16:19:21 +0530

    Add files via upload

---
+++ sabh1/sabh/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Sabh
+{
+     class ValRefTypes
+    {
+        public static void Main()
+        {
+            int i = 100;
+            int j;
+            j = i;
+            Console.WriteLine(j);
+            ++i;
+            Console.WriteLine(j);
+            int[] arr1 = { 1, 2, 3 };
+            int[] arr2;
+            arr2 = arr1;
+            arr1[0]++;
+            Console.WriteLine(arr2[0]);
+            Console.WriteLine(arr1[0]);
+        }
+    }
+}
